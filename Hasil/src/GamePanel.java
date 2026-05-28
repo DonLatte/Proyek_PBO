1c584ab4d5278c12f2e4777f8d61ee9ad8f4f8b9
hansencoy <immanuelkusuma3@gmail.com>
2026-05-28 16:12:18 +0700

    Gambar Map 1

--- Hasil/src/GamePanel.java
+++ Hasil/src/GamePanel.java
@@ -7,19 +7,78 @@ import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel implements Runnable, KeyListener {
 
-    final int screenWidth = 800;
-    final int screenHeight = 600;
+    final int tileSize = 32;
+    
+    // MAP OPEN-PARK: 25 Baris x 47 Kolom
+    // 1 = Dinding/Bangunan Wahana (Putih)
+    // 0 = Jalan Utama/Area Terbuka (Hitam)
+    int[][] map = {
+
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+
+    };
+
+    final int screenWidth = 1504; 
+    final int screenHeight = 800; 
 
     Thread gameThread;
 
-    int playerX = 100;
-    int playerY = 100;
+    // Posisi awal player di area tengah bawah (Main Entrance)
+    int playerX = 736;
+    int playerY = 736;
     int playerSpeed = 4;
 
     boolean upPressed, downPressed, leftPressed, rightPressed;
 
     public GamePanel() {
-
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
@@ -37,11 +96,8 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 
     @Override
     public void run() {
-
         while (gameThread != null) {
-
             update();
-
             repaint();
 
             try {
@@ -53,21 +109,30 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
     }
 
     public void update() {
+        int nextX = playerX;
+        int nextY = playerY;
 
-        if (upPressed) {
-            playerY -= playerSpeed;
-        }
+        if (upPressed)    nextY -= playerSpeed;
+        if (downPressed)  nextY += playerSpeed;
+        if (leftPressed)  nextX -= playerSpeed;
+        if (rightPressed) nextX += playerSpeed;
 
-        if (downPressed) {
-            playerY += playerSpeed;
-        }
+        int leftCol = nextX / tileSize;
+        int rightCol = (nextX + tileSize - 1) / tileSize;
+        int topRow = nextY / tileSize;
+        int bottomRow = (nextY + tileSize - 1) / tileSize;
 
-        if (leftPressed) {
-            playerX -= playerSpeed;
-        }
+        if (topRow >= 0 && bottomRow < map.length && leftCol >= 0 && rightCol < map[0].length) {
+            
+            boolean collision = (map[topRow][leftCol] == 1 ||
+                                 map[topRow][rightCol] == 1 ||
+                                 map[bottomRow][leftCol] == 1 ||
+                                 map[bottomRow][rightCol] == 1);
 
-        if (rightPressed) {
-            playerX += playerSpeed;
+            if (!collision) {
+                playerX = nextX;
+                playerY = nextY;
+            }
         }
     }
 
@@ -75,57 +140,41 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(Color.white);
+        // Menggambar Map
+        for (int row = 0; row < map.length; row++) {
+            for (int col = 0; col < map[row].length; col++) {
+                if (map[row][col] == 1) {
+                    g.setColor(Color.white); // Bangunan / Pembatas / Rintangan
+                } else {
+                    g.setColor(Color.black); // Area Jalan Terbuka
+                }
+                g.fillRect(col * tileSize, row * tileSize, tileSize, tileSize);
+            }
+        }
 
-        g.fillRect(playerX, playerY, 32, 32);
+        // Menggambar Player (Kotak Merah)
+        g.setColor(Color.red);
+        g.fillRect(playerX, playerY, tileSize, tileSize);
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-
         int code = e.getKeyCode();
-
-        if (code == KeyEvent.VK_W) {
-            upPressed = true;
-        }
-
-        if (code == KeyEvent.VK_S) {
-            downPressed = true;
-        }
-
-        if (code == KeyEvent.VK_A) {
-            leftPressed = true;
-        }
-
-        if (code == KeyEvent.VK_D) {
-            rightPressed = true;
-        }
+        if (code == KeyEvent.VK_W) upPressed = true;
+        if (code == KeyEvent.VK_S) downPressed = true;
+        if (code == KeyEvent.VK_A) leftPressed = true;
+        if (code == KeyEvent.VK_D) rightPressed = true;
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
         int code = e.getKeyCode();
-
-        if (code == KeyEvent.VK_W) {
-            upPressed = false;
-        }
-
-        if (code == KeyEvent.VK_S) {
-            downPressed = false;
-        }
-
-        if (code == KeyEvent.VK_A) {
-            leftPressed = false;
-        }
-
-        if (code == KeyEvent.VK_D) {
-            rightPressed = false;
-        }
+        if (code == KeyEvent.VK_W) upPressed = false;
+        if (code == KeyEvent.VK_S) downPressed = false;
+        if (code == KeyEvent.VK_A) leftPressed = false;
+        if (code == KeyEvent.VK_D) rightPressed = false;
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
+    public void keyTyped(KeyEvent e) {}
 }
