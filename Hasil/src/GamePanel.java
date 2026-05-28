2569523ab0c8bd4caf64202784042979ab9d4187
hansencoy <immanuelkusuma3@gmail.com>
2026-05-28 15:08:06 +0700

    GamePanel2

--- Hasil/src/GamePanel.java
+++ Hasil/src/GamePanel.java
@@ -1,16 +1,131 @@
 import javax.swing.JPanel;
 import java.awt.Dimension;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable, KeyListener {
 
     final int screenWidth = 800;
     final int screenHeight = 600;
+
+    Thread gameThread;
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    boolean upPressed, downPressed, leftPressed, rightPressed;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+
+        this.addKeyListener(this);
+        this.setFocusable(true);
+
+        startGameThread();
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        while (gameThread != null) {
+
+            update();
+
+            repaint();
+
+            try {
+                Thread.sleep(16);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void update() {
+
+        if (upPressed) {
+            playerY -= playerSpeed;
+        }
+
+        if (downPressed) {
+            playerY += playerSpeed;
+        }
+
+        if (leftPressed) {
+            playerX -= playerSpeed;
+        }
+
+        if (rightPressed) {
+            playerX += playerSpeed;
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.white);
+
+        g.fillRect(playerX, playerY, 32, 32);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+
+        if (code == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+
+        if (code == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+
+        if (code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W) {
+            upPressed = false;
+        }
+
+        if (code == KeyEvent.VK_S) {
+            downPressed = false;
+        }
+
+        if (code == KeyEvent.VK_A) {
+            leftPressed = false;
+        }
+
+        if (code == KeyEvent.VK_D) {
+            rightPressed = false;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
     }
 }
